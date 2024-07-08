8f108e1140560a3eadab29fcbc8da3f32be4e65e
bymrbanks <bymrbanks@gmail.com>
2024-07-08 03:36:25 -0500

    first commit

---
+++ database/migrations/2024_07_08_081942_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('user_id'); // Foreign key to the users table
+            $table->string('file_id')->nullable(); // Figma file ID
+            $table->dateTime('last_imported_date')->nullable(); // Last imported date
+            $table->timestamps();
+        
+            // Foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
